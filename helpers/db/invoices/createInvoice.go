ba4d66177211227c21d364014779d78360c167ee
shivam.jha <shivam.jha@trell.in>
2022-04-13 11:52:40 +0530

    add products in invoices

--- helpers/db/invoices/createInvoice.go
+++ helpers/db/invoices/createInvoice.go
@@ -14,10 +14,17 @@ func CreateInvoiceDAO(ctx context.Context, createInvoiceRequest *request.CreateI
 	}
 	arrayString = arrayString[:len(arrayString)-1]
 	arrayString += ")"
-	sqlString := fmt.Sprintf("INSERT INTO invoices (user_id,selling_company_id,buying_company_id,grand_total) SELECT \"%v\",\"%v\",\"%v\",sum(productprice) from product where idproduct in %v", createInvoiceRequest.UserID, createInvoiceRequest.SellingCompanyID, createInvoiceRequest.BuyingCompanyID, arrayString)
-	_, err := services.Dbmap.Exec(sqlString)
+	var products []string
+	sqlString := fmt.Sprintf("SELECT productname from product where idproduct in  %v", arrayString)
+	_, err := services.Dbmap.Select(&products, sqlString)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err.Error(), sqlString)
+		return "Can't create Invoice"
+	}
+	sqlString = fmt.Sprintf("INSERT INTO invoices (user_id,selling_company_id,buying_company_id,products,grand_total) SELECT \"%v\",\"%v\",\"%v\",\"%v\",sum(productprice) from product where idproduct in %v", createInvoiceRequest.UserID, createInvoiceRequest.SellingCompanyID, createInvoiceRequest.BuyingCompanyID, products, arrayString)
+	_, err = services.Dbmap.Exec(sqlString)
+	if err != nil {
+		fmt.Println(err.Error(), sqlString)
 		return "Can't create Invoice"
 	}
 	return ""
